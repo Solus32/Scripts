f69452a73c13118f7405258d018001911398acb2
José Luis <Solus32@users.noreply.github.com>
2019-03-31 14:55:26 -0600

    Add files via upload

--- Extraer-tar-bz2.sh
+++ Extraer-tar-bz2.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+echo
+echo "\e[93mSe Descomprimirán los archivos tar.bz2"
+echo "\e[0m"
 tar -xvjf *.tar.bz2
+echo
+echo "\e[93mDescompresión Terminada"
+echo
--- Extraer-tar-gz.sh
+++ Extraer-tar-gz.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+echo
+echo "\e[93mSe Descomprimirán los archivos tar.gz"
+echo "\e[0m"
 tar -xvzf *.tar.gz
+echo
+echo "\e[93mDescompresión Terminada"
+echo
