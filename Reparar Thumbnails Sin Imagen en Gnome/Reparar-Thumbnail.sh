8f5fc5dcf65c3418a3645789d15ff0a77910794c
José Luis <Solus32@users.noreply.github.com>
2020-02-03 11:17:59 -0600

    Add files via upload

---
+++ Reparar Thumbnails Sin Imagen en Gnome/Reparar-Thumbnail.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+rm -r ~/.cache/thumbnails/fail
+mkdir -p ~/.cache/thumbnails/fail
+nautilus -q
---
+++ Reparar Thumbnails Sin Imagen en Gnome/Script.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sudo cp Reparar-Thumbnail.sh /usr/local/bin/reparar-thumbnail
+sudo chmod +x /usr/local/bin/reparar-thumbnail
+cd /usr/local/bin
+sudo chown root:root *
